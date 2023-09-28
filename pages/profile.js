dcad58053acb5d0fd461a22d25a736d5a2d38ba6
Pato-web-development <patrickmaebi@gmail.com>
2023-09-28 12:16:41 +0100

    profile

--- pages/profile.js
+++ pages/profile.js
@@ -3,9 +3,10 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Footer from "@/components/footer";
 
 export default function Profile() {
-    
+
     useEffect(() => {
         AOS.init();
       }, [])
@@ -19,8 +20,14 @@ export default function Profile() {
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/svg-with-js.min.css" />;
    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
     </Head>
-        <div className="p-[5%] bg-[#234]">
-        <div className="">we up</div>
+        <div>
+        <div className="p-[5%] profilebackground" style={{backgroundImage:"url(images/bg3.jpg)", backgroundSize:"100%", backgroundPositionY:"90%"}}>
+        <div className="lg:text-[140%] md:text-[130%] text-[120%] font-[500]"><Link href="/"><i className="fa fa-caret-left"></i> &nbsp; <span style={{textShadow:"2px 2px #000"}}>RETURN</span></Link></div>
+            <div className="pt-[5%]"><i className="fa fa-user-circle lg:text-[450%] md:text-[400%] text-[300%]"></i></div>
+            <div className="mt-[0.7cm]"><span className="bg-[#00c] px-[0.5cm] py-[0.2cm] font-[600] rounded-md">Connect Wallet</span></div>
+        </div>
+        <div></div>
+        <Footer />
         </div>
         </>
     )

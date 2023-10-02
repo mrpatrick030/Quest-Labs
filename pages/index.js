e6f52f3f55b303876267f84584fbcae3ab874447
Pato-web-development <patrickmaebi@gmail.com>
2023-10-02 14:57:44 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -33,13 +33,14 @@ export default function Home(){
     <ProjectHeader />
     <div className='pb-[10%] grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 px-[5%] lg:mt-[2%] md:mt-[2%] mt-[10%] gap-2'>
     <div className='grid-cols-1 text-[200%] lg:text-[350%] md:text-[250%] lg:mr-[40%] md:mr-[30%] lg:mt-[3%] md:mt-[3%] font-[600] lg:text-left md:text-left text-center'>
-    Mass adoption of token-governed technologies
+    Widespread use of token-governed technology
     </div>
     <div className='grid-cols-1 text-[100%] lg:text-[130%] md:text-[110%] lg:ml-[45%] md:ml-[35%] mt-[4%] lg:text-left md:text-left text-center'>
-   <div className='firstsectionshadowdiv p-[0.5cm] bg-[rgba(0,0,0,0.4)]' data-aos="flip-up" style={{transition:"1s ease-in-out"}}>With Quest Labs Network, an Ethereum Layer 2 based DAO, Quest Labs Treasury and a token holder governed roadmap for
-     products and initiatives.</div>
+   <div className='firstsectionshadowdiv p-[0.5cm] bg-[rgba(0,0,0,0.4)]' data-aos="flip-up" style={{transition:"1s ease-in-out"}}>An Ethereum Layer 2 based DAO, $QTK, 
+   QUESTNFT, Quest Labs Treasury, token holder guided roadmap, and Proposals for products and initiatives are all 
+   part of the Quest Labs Network.</div>
      <div className='mt-[10%]'>
-      <Link href="https://www.ayalab.xyz/all-projects"><button onClick={ChangeColor} className='docsbutton p-[0.3cm] rounded-md bg-[#002] cursor-pointer' style={{border:"2px solid #fff", backgroundColor:buttonColor}}>View Docs</button></Link>
+      <Link href="https://github.com/mrpatrick030/Quest-Labs/blob/main/README.md"><button onClick={ChangeColor} className='docsbutton p-[0.3cm] rounded-md bg-[#002] cursor-pointer' style={{border:"2px solid #fff", backgroundColor:buttonColor}}>View Docs</button></Link>
     </div>
     </div>
     </div>
